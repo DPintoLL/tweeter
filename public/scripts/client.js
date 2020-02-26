06a3826d33f29ad47d09eb5e880d9fbf6038b081
Diogo Pinto <diogosmp@gmail.com>
2020-02-26 22:33:08 +0000

    Implemented Form Validation. Updating TimeStamps WIP

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,59 +1,31 @@
-// jQuery's document ready function:
-$(() => {
-  // import Moment library to handle time
-
-  // const tweetData = [
-  //   {
-  //     user: {
-  //       name: "Newton",
-  //       avatars: "https://i.imgur.com/73hZDYK.png",
-  //       handle: "@SirIsaac"
-  //     },
-  //     content: {
-  //       text:
-  //         "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     created_at: 1461116232227
-  //   },
-  //   {
-  //     user: {
-  //       name: "Descartes",
-  //       avatars: "https://i.imgur.com/nlhLi3I.png",
-  //       handle: "@rd"
-  //     },
-  //     content: {
-  //       text: "Je pense , donc je suis"
-  //     },
-  //     created_at: 1461113959088
-  //   }
-  // ];
-
-  const createTweetElement = tweetData => {
-    const article = `
-    <article class="tweet-card">
-      <header>
-        <img class="user-img" src="${
-          tweetData.user.avatars
-        }" alt="user avatar"/>
-        <span class="user-name">${tweetData.user.name}</span>
-        <span class="alias">${tweetData.user.handle}</span>
+// Function Definitions
+const createTweetElement = tweetData => {
+  // Using Moment library to handle date formatting
+  const article = `
+  <article class="tweet-card">
+  <header>
+  <img class="user-img" src="${tweetData.user.avatars}" alt="user avatar"/>
+      <span class="user-name">${tweetData.user.name}</span>
+      <span class="alias">${tweetData.user.handle}</span>
       </header>
       <div class="tweet-body">
-        <p>${tweetData.content.text}</p>
+      <p>${tweetData.content.text}</p>
       </div>
       <footer>
-        <span class="days-ago">${moment(tweetData.created_at).fromNow()}</span>
-        <div class="icons">
-          <i class="fas fa-flag"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="fas fa-heart"></i>
-        </div>
+      <span class="days-ago">${moment(tweetData.created_at).fromNow()}</span>
+      <div class="icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+      </div>
       </footer>
-    </article>`;
+      </article>`;
 
-    return article;
-  };
+  return article;
+};
 
+// jQuery's document ready function:
+$(() => {
   const renderTweets = tweetArr => {
     tweetArr.forEach(element => {
       const newTweet = createTweetElement(element);
@@ -61,23 +33,43 @@ $(() => {
     });
   };
 
-  // AJAX POST request that sends the form data (new tweets) to the server.
-  $(function() {
-    // Selects the Form
-    const $newTweetForm = $("#submit-form");
+  //? - NTS: This function is not invoked explicitly, only when the page is loaded (by jQuery's $(document).ready). Should it be invoked somewhere?
+  // $(function() {
+  // });
 
-    // Add eventListener and prevent default
-    $newTweetForm.submit(function(event) {
-      event.preventDefault();
-      console.log("Button clicked, performing ajax call...");
+  // EventListener for Form Submission. AJAX POST request adds new tweets to DB
+  const $newTweetForm = $("#submit-form");
+  $newTweetForm.submit(function(event) {
+    // Selects the Form and the contents of the textarea
+    const $textArea = $("#submit-form textarea").val();
 
-      // AJAX method
-      $.ajax("/tweets", {
-        method: "POST",
-        data: $newTweetForm.serialize()
-      }).then(function() {
-        console.log("Successfully posted new tweet to /tweets");
-      });
+    // Prevents page reload behaviour
+    event.preventDefault();
+
+    console.log("Button clicked, performing ajax call...");
+
+    // Performs basic validation on textarea text-content
+    if ($textArea.length > 140 || $textArea === "" || $textArea === null) {
+      console.log("Bad request.");
+      alert("Please write a Tweetish up to 140 characters.");
+      return;
+    }
+
+    //! TO-DO -----------------------------------------------------
+    //! Capture and update timestamps of previous tweets ----------
+    // const timeStampsArr = document.querySelectorAll("timeStamps");
+    const $timeStampsArr = $(".days-ago");
+    // timeStampsArr.forEach(el => (el.val = moment(el.val).fromNow()));
+    //! -----------------------------------------------------------
+
+    // AJAX request that adds new tweet to DB
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: $newTweetForm.serialize()
+    }).then(function() {
+      console.log("Successfully posted new tweet to /tweets");
+      loadTweets();
     });
   });
 
@@ -85,7 +77,11 @@ $(() => {
   const loadTweets = function() {
     $.ajax("/tweets", { method: "GET" }).then(tweets => {
       console.log("Successfully retrieved tweets from GET/tweets ", tweets);
-      renderTweets(tweets);
+
+      // Right now renders the single latest tweet back to be prepended
+      const newTweetToBeRendered = [];
+      newTweetToBeRendered.push(tweets[tweets.length - 1]);
+      renderTweets(newTweetToBeRendered);
     });
   };
 
