07791e655142a61b64a05df1c5a80110f59a538d
Diogo Pinto <diogosmp@gmail.com>
2020-02-26 04:58:53 +0000

    Finalized posting and fetching tweets from database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,55 +2,53 @@
 $(() => {
   // import Moment library to handle time
 
-  const tweetData = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac"
-      },
-      content: {
-        text:
-          "If I have seen further it is by standing on the shoulders of giants"
-      },
-      created_at: 1461116232227
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd"
-      },
-      content: {
-        text: "Je pense , donc je suis"
-      },
-      created_at: 1461113959088
-    }
-  ];
+  // const tweetData = [
+  //   {
+  //     user: {
+  //       name: "Newton",
+  //       avatars: "https://i.imgur.com/73hZDYK.png",
+  //       handle: "@SirIsaac"
+  //     },
+  //     content: {
+  //       text:
+  //         "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     created_at: 1461116232227
+  //   },
+  //   {
+  //     user: {
+  //       name: "Descartes",
+  //       avatars: "https://i.imgur.com/nlhLi3I.png",
+  //       handle: "@rd"
+  //     },
+  //     content: {
+  //       text: "Je pense , donc je suis"
+  //     },
+  //     created_at: 1461113959088
+  //   }
+  // ];
 
-  // <i class="user-img fas fa-user-astronaut fa-2x"></i>
   const createTweetElement = tweetData => {
-    // typeof tweet === 'object'
     const article = `
     <article class="tweet-card">
-    <header>
-    <img class="user-img" src="${tweetData.user.avatars}" alt="user avatar"/>
-    <span class="user-name">${tweetData.user.name}</span>
-    <span class="alias">${tweetData.user.handle}</span>
-    </header>
-
-    <div class="tweet-body">
-    <p>${tweetData.content.text}</p>
-    </div>
-  
-    <footer>
-      <span class="days-ago">${moment(tweetData.created_at).fromNow()}</span>
-      <div class="icons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
+      <header>
+        <img class="user-img" src="${
+          tweetData.user.avatars
+        }" alt="user avatar"/>
+        <span class="user-name">${tweetData.user.name}</span>
+        <span class="alias">${tweetData.user.handle}</span>
+      </header>
+      <div class="tweet-body">
+        <p>${tweetData.content.text}</p>
       </div>
-    </footer>
+      <footer>
+        <span class="days-ago">${moment(tweetData.created_at).fromNow()}</span>
+        <div class="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
     </article>`;
 
     return article;
@@ -63,5 +61,33 @@ $(() => {
     });
   };
 
-  renderTweets(tweetData);
+  // AJAX POST request that sends the form data (new tweets) to the server.
+  $(function() {
+    // Selects the Form
+    const $newTweetForm = $("#submit-form");
+
+    // Add eventListener and prevent default
+    $newTweetForm.submit(function(event) {
+      event.preventDefault();
+      console.log("Button clicked, performing ajax call...");
+
+      // AJAX method
+      $.ajax("/tweets", {
+        method: "POST",
+        data: $newTweetForm.serialize()
+      }).then(function() {
+        console.log("Successfully posted new tweet to /tweets");
+      });
+    });
+  });
+
+  // Fetches tweets from the JSON DB through the GET/tweets endpoint
+  const loadTweets = function() {
+    $.ajax("/tweets", { method: "GET" }).then(tweets => {
+      console.log("Successfully retrieved tweets from GET/tweets ", tweets);
+      renderTweets(tweets);
+    });
+  };
+
+  loadTweets();
 });
